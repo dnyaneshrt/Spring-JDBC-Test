de3a779f4167203b3bf6119aa9a207925ed6a113
Dnyaneshwar <sartape51@gmail.com>
2024-12-18 20:56:50 +0530

    initial code added

---
+++ src/test/java/com/learn/Spring_JDBC/Spring_JDBC/SpringJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.Spring_JDBC.Spring_JDBC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
